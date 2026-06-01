bfac933ceadfe12a9cf79a8be941a63ef53fef70
Vorbanux <bobylevv472@gmail.com>
2026-06-01 15:36:00 +0300

    Update auto_update.py

--- auto_update.py
+++ auto_update.py
@@ -22,3 +22,31 @@ def get_Steam_data():
     print(f"Ошибка при запросе к Steam API: {e}")
 
   return "<div align='center'>💤 не в сети</div>"
+
+def update_readme(status_html):
+    with open("README.md", "r", encoding="utf-8") as f:
+        readme = f.read()
+   
+    start_marker = "<!-- STEAM_STATUS:START -->"
+    end_marker = "<!-- STEAM_STATUS:END -->"
+    
+    start_pos = readme.find(start_marker)
+    end_pos = readme.find(end_marker)
+    
+    # Если маркеры успешно найдены в файле README.md
+    if start_pos != -1 and end_pos != -1:
+        new_readme = (
+            readme[:start_pos + len(start_marker)] 
+            + "\n" + status_html + "\n" + 
+            readme[end_pos:]
+        )
+        with open("README.md", "w", encoding="utf-8") as f:
+            f.write(new_readme)
+    else:
+        print("Ошибка: Маркеры не найдены в README.md! Проверьте разметку.")
+if __name__ == "__main__":
+    if STEAM_API_KEY and STEAM_ID:
+        current_status = get_steam_data()
+        update_readme(current_status)
+    else:
+        print("Ошибка: Переменные окружения STEAM_API_KEY или STEAM_ID не найдены.")
